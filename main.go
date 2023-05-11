0a4e91116c2d8de9901254b57c22b893a8decc2d
nach9 <nur.achdiansyah@gmail.com>
2023-05-11 17:11:33 +0700

    feat: add graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/nach9/go-todolist/pkg/activities"
@@ -11,6 +17,12 @@ import (
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	gin.SetMode(gin.ReleaseMode)
+
+	appPort := "3030"
+
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
 
@@ -28,5 +40,29 @@ func main() {
 	activities.RegisterRoutes(r, db)
 	todos.RegisterRoutes(r, db)
 
-	r.Run(":3030")
+	srv := &http.Server{
+		Addr:    ":" + appPort,
+		Handler: r,
+	}
+
+	log.Println("Listening and serving HTTP on PORT", appPort)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
